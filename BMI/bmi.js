17d30a2da66d0ddc5a6039533f8d013b8faf405e
InfoTech <infotech@romits-MacBook-Pro.local>
2019-01-27 16:13:51 -0500

    project added

---
+++ BMI/bmi.js
@@ -0,0 +1,28 @@
+function calcBmi() {
+    let lb, kg;
+   // kg = prompt('what is your weight in lb?') * .45
+    console.log(kg);
+    
+}
+
+calcBmi()
+
+
+
+
+/*
+
+Metric BMI Formula
+BMI = weight (kg) ÷ height2 (m2)
+
+Imperial BMI Formula
+BMI = weight (lb) ÷ height2 (in2) × 703
+
+    let bmi = (weight) / ((feet * 12) + (inches) * 703)
+
+*/
+
+
+
+
+
